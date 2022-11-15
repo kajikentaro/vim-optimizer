94c026af6ff28bdbe5e52179c933ad8e27ecbdfa
kajikentaro <kajindowsxp@gmail.com>
2022-11-15 13:45:18 +0900

    Test: iwやi)など最初に実行してはいけないケースを弾く関数をテスト実装

--- test/preprocess.ts
+++ test/preprocess.ts
@@ -158,8 +158,76 @@ export async function preprocess() {
   const startIdx = parseInt(process.env.START || '0');
   const endIdx = parseInt(process.env.END || allActions.length + '');
 
+  const isBanFirstAction = createUnreachableActionTree(allActions, allActionKeys);
   startIdx === 0 && (await preprocessSingleAction(allActions, allActionKeys));
   await preprocessDoubleAction(allActions, allActionKeys, startIdx, endIdx);
 
   console.log('done preprocessing');
+}
+
+// aw, i)などの範囲指定コマンドは、オペレーターがない状態で初回実行できない。
+// 初回実行を想定していないアクションを抽出するため、
+// ノードがアクションキーとなる木を作り、「doesActionApplyがtrueとなるアクションの子」をメモしておくことでこれを防ぐ
+function createUnreachableActionTree(allActions: BaseAction[], allActionKeys: string[][]) {
+  class Node {
+    key: string;
+    nextNode: Map<string, Node>;
+
+    // 木を作った後に使用する値
+    actionIdx: number[];
+
+    constructor(key: string) {
+      this.key = key;
+      this.nextNode = new Map<string, Node>();
+      this.actionIdx = [];
+    }
+
+    getNext(key: string): Node {
+      const nextNode = this.nextNode.get(key);
+      if (typeof nextNode !== 'undefined') {
+        return nextNode;
+      }
+      const newNode = new Node(key);
+      this.nextNode.set(key, newNode);
+      return newNode;
+    }
+  }
+
+  // 木を作る
+  const firstNode = new Node('FIRST');
+  for (let i = 0; i < allActions.length; i++) {
+    const actionKey = allActionKeys[i];
+    let now = firstNode.getNext(actionKey[0]);
+    for (let j = 0; j < actionKey.length; i++) {
+      if (j === 0) continue;
+      now = now.getNext(actionKey[j]);
+    }
+    now.actionIdx.push(i);
+  }
+
+  function examineDoesActionApply(action: BaseAction, key: string[]): boolean {
+    return true;
+  }
+
+  const isBanFirstActions = new Array<boolean>(allActions.length).fill(false);
+  const dfs = (now: Node, didAction: boolean) => {
+    // アクションがすでに実行されていた場合は結果の配列に追加
+    for (const idx of now.actionIdx) {
+      isBanFirstActions[idx] = didAction;
+    }
+
+    // このノードでアクションが実行されるか調べる
+    let doAction = didAction;
+    for (const idx of now.actionIdx) {
+      doAction = doAction || examineDoesActionApply(allActions[idx], allActionKeys[idx]);
+    }
+
+    // 再帰
+    for (const [_, nextNode] of now.nextNode) {
+      dfs(nextNode, doAction);
+    }
+  };
+  dfs(firstNode, false);
+
+  return isBanFirstActions;
 }

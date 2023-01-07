a3fb36da4f5a0d709b745bf23251b2cb48c01dce
kajikentaro <kajindowsxp@gmail.com>
2023-01-07 05:42:58 -0800

    Add: テストコードを追加

--- wasm/controller/operations_test.go
+++ wasm/controller/operations_test.go
@@ -19,32 +19,188 @@ var sampleEditorText_1 = []string{
 	"}",
 }
 
-func TestGenMoveToMatchingBracketForward(t *testing.T) {
+func TestGenMoveToMatchingBracket(t *testing.T) {
 	action := genMoveToMatchingBracket()
-	origin := model.Position{Line: 2, Character: 10}
-	expected := model.Position{Line: 9, Character: 0}
 
-	actual := action.run(sampleEditorText_1, origin)
-
-	if actual.Line != expected.Line {
-		t.Errorf("Got Line = %d, want %d", actual.Line, expected.Line)
+	tests := []struct {
+		name     string
+		origin   model.Position
+		expected model.Position
+	}{
+		{
+			name:     "search forward brackets",
+			origin:   model.Position{Line: 2, Character: 10},
+			expected: model.Position{Line: 9, Character: 0},
+		},
+		{
+			name:     "search backward brackets",
+			origin:   model.Position{Line: 4, Character: 19},
+			expected: model.Position{Line: 4, Character: 5},
+		},
 	}
-	if actual.Character != expected.Character {
-		t.Errorf("Got Character = %d, want %d", actual.Character, expected.Character)
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actual := action.run(sampleEditorText_1, test.origin)
+			if actual.Line != test.expected.Line {
+				t.Errorf("Got Line = %d, want %d", actual.Line, test.expected.Line)
+			}
+			if actual.Character != test.expected.Character {
+				t.Errorf("Got Character = %d, want %d", actual.Character, test.expected.Character)
+			}
+		})
 	}
 }
 
-func TestGenMoveToMatchingBracketBackward(t *testing.T) {
-	action := genMoveToMatchingBracket()
-	origin := model.Position{Line: 4, Character: 19}
-	expected := model.Position{Line: 4, Character: 5}
+func TestGenMoveUp(t *testing.T) {
+	action := genMoveUp()
 
-	actual := action.run(sampleEditorText_1, origin)
-
-	if actual.Line != expected.Line {
-		t.Errorf("Got Line = %d, want %d", actual.Line, expected.Line)
+	tests := []struct {
+		name     string
+		origin   model.Position
+		expected model.Position
+	}{
+		{
+			name:     "execute top of line",
+			origin:   model.Position{Line: 0, Character: 5},
+			expected: model.Position{Line: 0, Character: 5},
+		},
+		{
+			name:     "move up normally",
+			origin:   model.Position{Line: 1, Character: 5},
+			expected: model.Position{Line: 0, Character: 5},
+		},
+		{
+			name:     "move up from out of text",
+			origin:   model.Position{Line: 3, Character: 20},
+			expected: model.Position{Line: 2, Character: 20},
+		},
 	}
-	if actual.Character != expected.Character {
-		t.Errorf("Got Character = %d, want %d", actual.Character, expected.Character)
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actual := action.run(sampleEditorText_1, test.origin)
+			if actual.Line != test.expected.Line {
+				t.Errorf("Got Line = %d, want %d", actual.Line, test.expected.Line)
+			}
+			if actual.Character != test.expected.Character {
+				t.Errorf("Got Character = %d, want %d", actual.Character, test.expected.Character)
+			}
+		})
+	}
+
+}
+
+func TestGenMoveDown(t *testing.T) {
+	action := genMoveDown()
+
+	tests := []struct {
+		name     string
+		origin   model.Position
+		expected model.Position
+	}{
+		{
+			name:     "execute bottom of line",
+			origin:   model.Position{Line: 9, Character: 0},
+			expected: model.Position{Line: 9, Character: 0},
+		},
+		{
+			name:     "move down normally",
+			origin:   model.Position{Line: 1, Character: 5},
+			expected: model.Position{Line: 2, Character: 5},
+		},
+		{
+			name:     "move down from out of text",
+			origin:   model.Position{Line: 3, Character: 20},
+			expected: model.Position{Line: 4, Character: 20},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actual := action.run(sampleEditorText_1, test.origin)
+			if actual.Line != test.expected.Line {
+				t.Errorf("Got Line = %d, want %d", actual.Line, test.expected.Line)
+			}
+			if actual.Character != test.expected.Character {
+				t.Errorf("Got Character = %d, want %d", actual.Character, test.expected.Character)
+			}
+		})
+	}
+}
+
+func TestGenMoveRight(t *testing.T) {
+	action := genMoveRight()
+
+	tests := []struct {
+		name     string
+		origin   model.Position
+		expected model.Position
+	}{
+		{
+			name:     "move right normally",
+			origin:   model.Position{Line: 1, Character: 5},
+			expected: model.Position{Line: 1, Character: 6},
+		},
+		{
+			name:     "move right from out of the text",
+			origin:   model.Position{Line: 3, Character: 20},
+			expected: model.Position{Line: 3, Character: 10},
+		},
+		{
+			name:     "move right from end of the line",
+			origin:   model.Position{Line: 3, Character: 10},
+			expected: model.Position{Line: 3, Character: 10},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actual := action.run(sampleEditorText_1, test.origin)
+			if actual.Line != test.expected.Line {
+				t.Errorf("Got Line = %d, want %d", actual.Line, test.expected.Line)
+			}
+			if actual.Character != test.expected.Character {
+				t.Errorf("Got Character = %d, want %d", actual.Character, test.expected.Character)
+			}
+		})
+	}
+}
+
+func TestGenMoveLeft(t *testing.T) {
+	action := genMoveLeft()
+
+	tests := []struct {
+		name     string
+		origin   model.Position
+		expected model.Position
+	}{
+		{
+			name:     "move left normally",
+			origin:   model.Position{Line: 1, Character: 5},
+			expected: model.Position{Line: 1, Character: 4},
+		},
+		{
+			name:     "move left from out of the text",
+			origin:   model.Position{Line: 3, Character: 20},
+			expected: model.Position{Line: 3, Character: 9},
+		},
+		{
+			name:     "move right from head of the line",
+			origin:   model.Position{Line: 3, Character: 0},
+			expected: model.Position{Line: 3, Character: 0},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actual := action.run(sampleEditorText_1, test.origin)
+			if actual.Line != test.expected.Line {
+				t.Errorf("Got Line = %d, want %d", actual.Line, test.expected.Line)
+			}
+			if actual.Character != test.expected.Character {
+				t.Errorf("Got Character = %d, want %d", actual.Character, test.expected.Character)
+			}
+		})
 	}
 }
